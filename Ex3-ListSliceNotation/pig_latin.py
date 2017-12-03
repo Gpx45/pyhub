4754683e44fd928e1134bbe6cc6d40dced76e0d3
Victor Martins <vich.martins@gmail.com>
2017-12-03 01:53:00 -0500

    added

---
+++ Ex3-ListSliceNotation/pig_latin.py
@@ -0,0 +1,8 @@
+# A pig latin generator
+
+phrase = input("Please Enter your phrase: ")
+plist = list(phrase)
+first_letter = plist.pop(0)
+plist.append(first_letter)
+new_phrase = ''.join(plist)
+print(new_phrase + 'ay')
