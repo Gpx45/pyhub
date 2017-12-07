0322304f0185429bbd1ed11751840cb63a63ba78
Victor Martins <vich.martins@gmail.com>
2017-12-06 23:52:27 -0500

    added

---
+++ Sets-Tuples/tuples.py
@@ -0,0 +1,13 @@
+# Tuples simply don't change, they're IMMUTABLE
+# heres are example of Tuples
+
+vowels = ('a','e','i','o','u')
+
+# If you store in item as a tuple with () you'll actually just get the type
+# of whatever you placed in the ()
+# ex.
+num = (1) # This isn't a Tuple
+ # There is a quirk in the language, you can store one item as a tuple
+ # ex.
+ num = (1,) # This registers as a tuple object because a rule of a tuple is that
+ # You need to have at least one trailing comma to make a sequence, therefore a tuple
