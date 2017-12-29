e192207cf273ee72d9b490bb1649a7015dd57fc7
Victor Martins <vich.martins@gmail.com>
2017-12-29 02:47:10 -0500

    added

--- web-app-flask/vsearch-web2.py
+++ web-app-flask/vsearch-web2.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request
 from vsearch import vsearch
 
 app = Flask(__name__) # These are special values kept by the interpreter. Values with __ are called dunders while _ is called a wonder double and one underscores.
 
-@app.route('/') # This is called a function decorator which always start with an @ a decorator ajusts the behavior of a function without you having to rewrite it.
-def hello() -> 302:
-    return redirect('/entry')
-
-
+# We can have more than one URL be associated with a function, this is so you don't have to import if you don't need to.
+@app.route('/')
 @app.route('/entry')
 def entry_page() ->'html':
     return render_template('entry.html',the_title='Welcome to search4letters Online')
@@ -24,8 +21,6 @@ def do_search() -> 'html':
         the_letters = letters,
         the_title = title,
         the_results = results,)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
