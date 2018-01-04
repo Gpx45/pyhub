f51be196479c0e69bebec38ca4e48d68c0003cb4
Victor Martins <vich.martins@gmail.com>
2018-01-04 00:55:49 -0500

    added

--- web-app-flask/vsearch-web2.py
+++ web-app-flask/vsearch-web2.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,escape
 from vsearch import vsearch
 
 app = Flask(__name__) # These are special values kept by the interpreter. Values with __ are called dunders while _ is called a wonder double and one underscores.
+
+
+def log_request(req: 'flask_request', res: str)-> None :
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, sep='|', file=log)
 
 # We can have more than one URL be associated with a function, this is so you don't have to import if you don't need to.
 @app.route('/')
@@ -16,11 +21,20 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = "Here are your results: "
     results = str(vsearch(phrase,letters))
+    log_request(request,results)
     return render_template('result.html',
         the_phrase = phrase,
         the_letters = letters,
         the_title = title,
         the_results = results,)
+
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return ''.join(escape(contents))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
