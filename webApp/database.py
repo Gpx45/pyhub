ece7edc83a9f55e62bb20dc80094a627a15d766d
Victor Martins <vich.martins@gmail.com>
2018-04-07 20:47:05 -0400

    added

--- webApp/database.py
+++ webApp/database.py
@@ -7,7 +7,7 @@ class Database:
     def __init__(self, config: dict) -> None: # The init dunder is the special method that initilizes all of the attributes of the class.
         self.configuration = config
 
-    def __enter__(self) -> Cursor: # enter dunder does the setup for the class in terms of a context manager.
+    def __enter__(self) -> 'Cursor': # enter dunder does the setup for the class in terms of a context manager.
         self.connect = pymysql.connect(**self.configuration) # Make sure to address the parameter as a list with the ** (Remenber * is for a tuple)
         self.cursor = self.connect.cursor()
         return self.cursor
