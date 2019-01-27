e9ed7fa76d6e7bf5872897d82e29a42742e095f8
Victor Martins <vich.martins@gmail.com>
2019-01-27 18:19:31 -0500

    added

--- comprehension loops/reading.py
+++ comprehension loops/reading.py
@@ -8,6 +8,22 @@ with open('buzzers.csv') as raw_data:
 """
 import csv
 
+"""
 with open('buzzers.csv') as data:
     for line in csv.reader(data):
         print(line)
+"""
+
+"""
+with open('buzzers.csv') as data:
+    for line in csv.DictReader(data):
+        print(line)
+"""
+
+with open('buzzers.csv') as data:
+    ignore = data.readline()
+    flights = {}
+    for line in data:
+        k, v = line.split(',')
+        flights[k] = v
+    print(flights)
