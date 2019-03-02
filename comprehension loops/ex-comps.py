423f8c1854b7eda16e98f967f44fc914102b8dea
Victor Martins <vich.martins@gmail.com>
2019-03-02 15:23:35 -0500

    added

--- comprehension loops/ex-comps.py
+++ comprehension loops/ex-comps.py
@@ -1,4 +1,4 @@
-
+from pprint import pprint
 
 data = [1, 2, 3, 4, 5, 6, 7, 8]
 
@@ -47,3 +47,29 @@ print(title)
 title = [word.title() for word in data]
 
 print(title)
+
+print("--------------------------------------------")
+
+
+with open('buzzers.csv') as data:
+    ignore = data.readline()
+    fts = {}
+    for line in data:
+        k, v = line.strip().split(',')
+        fts[k] = v.strip().title()
+
+west = [k for k, v in fts.items() if v == 'West End']
+
+print(fts)
+print(west)
+
+print("--------------------------------------------")
+
+dests = set(fts.values())
+
+#print(dests)
+
+when = {}
+for dests in set(fts.values()):
+    when[dests] = [k for k, v in fts.items() if v == dests]
+pprint(when)
