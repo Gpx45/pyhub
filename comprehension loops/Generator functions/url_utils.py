60f45d1a9472b94e092d7cf21cb870f38e008bf0
Gpx45 <vich.martins@gmail.com>
2019-03-07 20:58:28 -0500

    added

--- comprehension loops/Generator functions/url_utils.py
+++ comprehension loops/Generator functions/url_utils.py
@@ -1,10 +1,33 @@
 
 import requests
 
+
+# This is a generator
+# A generator releases the data as soon as its available
 def gen_from_urls(urls: tuple) -> tuple:
     for resp in (requests.get(url) for url in urls):
         yield len(resp.content), resp.status_code, resp.url
 
+#This is a listcomp
+# Unlike a generator it waits for all the data to be produced then it releases 
 def gen_from_urls_list(urls: tuple) -> tuple:
     for resp in [requests.get(url) for url in urls]:
         yield len(resp.content), resp.status_code, resp.url
+
+
+#-------------------------------------------------------------
+# Example (Try this in the interpreter and you will see.)
+# 
+# A generator
+# for i in (x*3 for x in [1,2,3,4,5]):
+#      print(i)  
+#
+# A listcomp
+# for i in [x*3 for x in [1,2,3,4,5]]:
+#      print(i)
+#
+#
+#
+#
+#
+#
