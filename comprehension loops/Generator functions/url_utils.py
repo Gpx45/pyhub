72d9090a679ccb7ec5c6a75d15a8f4799d75676b
Gpx45 <vich.martins@gmail.com>
2019-03-07 20:48:04 -0500

    added

---
+++ comprehension loops/Generator functions/url_utils.py
@@ -0,0 +1,10 @@
+
+import requests
+
+def gen_from_urls(urls: tuple) -> tuple:
+    for resp in (requests.get(url) for url in urls):
+        yield len(resp.content), resp.status_code, resp.url
+
+def gen_from_urls_list(urls: tuple) -> tuple:
+    for resp in [requests.get(url) for url in urls]:
+        yield len(resp.content), resp.status_code, resp.url
